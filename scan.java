6b69941a3501a14c749a12c1a3c1fff18a17fcc7
Suresh <itssuresh98@gmail.com>
2024-09-15 22:41:39 +0530

    Create scan.java

---
+++ scan.java
@@ -0,0 +1,13 @@
+public class Scan {
+
+    public static int nextInt() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'nextInt'");
+    }
+
+    public static void close() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'close'");
+    }
+
+}
